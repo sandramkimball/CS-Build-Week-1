406008b054c1adb3a2d060d99703d5602d907b7f
heiligbasil <basilief@yahoo.com>
2020-03-04 14:06:03 -0800

    Update Django models from Room to Chamber

--- adventure/models.py
+++ adventure/models.py
@@ -6,7 +6,7 @@ from rest_framework.authtoken.models import Token
 import uuid
 
 
-class Room(models.Model):
+class Chamber(models.Model):
     title = models.CharField(max_length=50, default="DEFAULT TITLE")
     description = models.CharField(max_length=500, default="DEFAULT DESCRIPTION")
     n_to = models.IntegerField(default=0)
@@ -16,53 +16,53 @@ class Room(models.Model):
     u_to = models.IntegerField(default=0)
     d_to = models.IntegerField(default=0)
 
-    def connectRooms(self, destinationRoom, direction):
-        destinationRoomID = destinationRoom.id
+    def connectChambers(self, destinationChamber, direction):
+        destinationChamberID = destinationChamber.id
         try:
-            destinationRoom = Room.objects.get(id=destinationRoomID)
-        except Room.DoesNotExist:
-            print("That room does not exist")
+            destinationChamber = Chamber.objects.get(id=destinationChamberID)
+        except Chamber.DoesNotExist:
+            print("That chamber does not exist")
         else:
             if direction == "n":
-                self.n_to = destinationRoomID
+                self.n_to = destinationChamberID
             elif direction == "s":
-                self.s_to = destinationRoomID
+                self.s_to = destinationChamberID
             elif direction == "e":
-                self.e_to = destinationRoomID
+                self.e_to = destinationChamberID
             elif direction == "w":
-                self.w_to = destinationRoomID
+                self.w_to = destinationChamberID
             elif direction == "u":
-                self.u_to = destinationRoomID
+                self.u_to = destinationChamberID
             elif direction == "d":
-                self.d_to = destinationRoomID
+                self.d_to = destinationChamberID
             else:
                 print("Invalid direction")
                 return
             self.save()
 
     def playerNames(self, currentPlayerID):
-        return [p.user.username for p in Player.objects.filter(currentRoom=self.id) if p.id != int(currentPlayerID)]
+        return [p.user.username for p in Player.objects.filter(currentChamber=self.id) if p.id != int(currentPlayerID)]
 
     def playerUUIDs(self, currentPlayerID):
-        return [p.uuid for p in Player.objects.filter(currentRoom=self.id) if p.id != int(currentPlayerID)]
+        return [p.uuid for p in Player.objects.filter(currentChamber=self.id) if p.id != int(currentPlayerID)]
 
 
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    currentRoom = models.IntegerField(default=0)
+    currentChamber = models.IntegerField(default=0)
     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
 
     def initialize(self):
-        if self.currentRoom == 0:
-            self.currentRoom = Room.objects.first().id
+        if self.currentChamber == 0:
+            self.currentChamber = Chamber.objects.first().id
             self.save()
 
-    def room(self):
+    def chamber(self):
         try:
-            return Room.objects.get(id=self.currentRoom)
-        except Room.DoesNotExist:
+            return Chamber.objects.get(id=self.currentChamber)
+        except Chamber.DoesNotExist:
             self.initialize()
-            return self.room()
+            return self.chamber()
 
 
 @receiver(post_save, sender=User)
