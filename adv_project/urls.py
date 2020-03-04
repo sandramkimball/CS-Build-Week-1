8d276d7ec093bba7c57733eb65a060ddf84d5871
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-03-03 17:02:31 -0800

    removed fe, removed djorg, added t oken-auth path

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/adv/', include('adventure.urls')),
+    path('api-token-auth/', views.obtain_auth_token)
 ]
